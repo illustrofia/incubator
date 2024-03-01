60ed0e6e1d1933bf4e6d1cb025872eb190f83cb5
Luca Airinei <illustrofia@gmail.com>
2024-03-01 09:24:31 +0100

    fix(ui): type exports

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -6,7 +6,7 @@ import dts from "vite-plugin-dts"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ outDir: "dist/types" })],
+  plugins: [react(), dts()],
   build: {
     sourcemap: true,
     lib: {

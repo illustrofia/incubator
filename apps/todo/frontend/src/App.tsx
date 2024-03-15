9863250086623144f2348c8a5bd3feee8139b60f
Luca Airinei <illustrofia@gmail.com>
2024-03-15 19:11:46 +0100

    chore(todo): cleanup

---
+++ apps/todo/frontend/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex min-h-screen w-full flex-col items-center justify-center gap-4">
+      Hello world!
+    </div>
+  )
+}
+
+export default App
--- apps/todo/triplit/src/client.ts
+++
@@ -1,9 +0,0 @@
-import { TriplitClient } from "@triplit/client"
-import { schema } from "../triplit/schema.js"
-
-export const triplit = new TriplitClient({
-  schema,
-  storage: "indexeddb",
-  token: import.meta.env.VITE_TRIPLIT_TOKEN,
-  serverUrl: import.meta.env.VITE_TRIPLIT_SERVER_URL,
-})

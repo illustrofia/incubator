967309d2021c926b71e1da5f7ea677dcd516d653
Luca Airinei <illustrofia@gmail.com>
2024-03-21 01:21:38 +0100

    chore(shared): user schema

---
+++ packages/shared/.eslintrc.cjs
@@ -0,0 +1,1 @@
+module.exports = require("@incubator/eslint-config/library.js")
---
+++ packages/shared/src/schemas/user.ts
@@ -0,0 +1,42 @@
+import { z } from "zod"
+
+export const userSchema = z
+  .object({
+    id: z.number(),
+    username: z
+      .string()
+      .min(3, { message: "Username must be at least 3 characters" }),
+    email: z.string().min(1, { message: "Email is required" }).email({
+      message: "Must be a valid email",
+    }),
+  })
+  .strip()
+
+export type UserSchema = z.infer<typeof userSchema>
+
+export const loginUserSchema = userSchema
+  .pick({ email: true })
+  .extend({
+    password: z
+      .string()
+      .min(6, { message: "Password must be at least 6 characters" }),
+  })
+  .strip()
+
+export type LoginUserSchema = z.infer<typeof loginUserSchema>
+
+export const registerUserSchema = userSchema
+  .pick({ username: true })
+  .merge(loginUserSchema)
+  .extend({
+    confirmPassword: z
+      .string()
+      .min(6, { message: "Password must be at least 6 characters" }),
+  })
+  .strip()
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  })
+
+export type RegisterUserSchema = z.infer<typeof registerUserSchema>

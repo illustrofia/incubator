536532c4a9517bcae58a2fe6617947e67d338dc4
Luca Airinei <illustrofia@gmail.com>
2024-03-23 19:09:00 +0100

    feat(todo backend): jwt authentication

--- apps/todo/backend/src/routes/auth.ts
+++ apps/todo/backend/src/routes/auth.ts
@@ -1,11 +1,22 @@
 import { prisma } from "@db/client"
 import { zValidator } from "@hono/zod-validator"
-import { registerUserSchema } from "@incubator/shared"
+import {
+  LoginUserSchema,
+  loginUserSchema,
+  registerUserSchema,
+} from "@incubator/shared"
 import argon2 from "argon2"
 import { Hono } from "hono"
+import { deleteCookie, setCookie } from "hono/cookie"
 import { HTTPException } from "hono/http-exception"
+import { sign } from "hono/jwt"
 
 export const auth = new Hono()
+
+const createToken = async (user: LoginUserSchema) =>
+  await sign({ email: user.email }, process.env.JWT_SECRET)
+
+const ONE_DAY = 1000 * 60 * 60 * 24
 
 auth.post(
   "/auth/register",
@@ -30,6 +41,21 @@ auth.post(
       },
     })
 
+    const token = await createToken(newUser)
+    setCookie(c, "token", token, {
+      httpOnly: true,
+      maxAge: ONE_DAY,
+      sameSite: "None",
+      secure: true,
+      domain: process.env.DOMAIN || "localhost",
+    })
+    setCookie(c, "hasAuthToken", "true", {
+      maxAge: ONE_DAY,
+      sameSite: "None",
+      secure: true,
+      domain: process.env.DOMAIN || "localhost",
+    })
+
     return c.json({
       id: newUser.id,
       username: newUser.username,
@@ -38,10 +64,61 @@ auth.post(
   },
 )
 
-auth.post("/auth/login", (c) => {
-  return c.text("login")
+auth.post("/auth/login", zValidator("json", loginUserSchema), async (c) => {
+  const { email, password } = c.req.valid("json")
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (!user) {
+    throw new HTTPException(401, { message: "User not found" })
+  }
+
+  const validPassword = await argon2.verify(user.password, password)
+  if (!validPassword) {
+    throw new HTTPException(401, { message: "Invalid password" })
+  }
+
+  const token = await createToken(user)
+
+  setCookie(c, "token", token, {
+    httpOnly: true,
+    maxAge: ONE_DAY,
+    sameSite: "None",
+    secure: true,
+    domain: process.env.DOMAIN || "localhost",
+  })
+  setCookie(c, "hasAuthToken", "true", {
+    maxAge: ONE_DAY,
+    sameSite: "None",
+    secure: true,
+    domain: process.env.DOMAIN || "localhost",
+  })
+
+  return c.json({
+    user: {
+      id: user.id,
+      username: user.username,
+      email: user.email,
+    },
+    message: "Logged in",
+  })
 })
 
-auth.post("/auth/logout", (c) => {
-  return c.text("logout")
+auth.post("/auth/logout", async (c) => {
+  deleteCookie(c, "token", {
+    httpOnly: true,
+    sameSite: "None",
+    secure: true,
+    domain: process.env.DOMAIN || "localhost",
+  })
+  deleteCookie(c, "hasAuthToken", {
+    sameSite: "None",
+    secure: true,
+    domain: process.env.DOMAIN || "localhost",
+  })
+
+  return c.json({ message: "Logged out" })
 })

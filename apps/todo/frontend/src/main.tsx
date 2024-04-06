34d2864ce04443a653bbf834c2c17241df9ddbf0
Luca Airinei <illustrofia@gmail.com>
2024-04-06 14:19:22 +0200

    fix(todo-frontend): fix casing in app.tsx

--- apps/todo/frontend/src/main.tsx
+++ apps/todo/frontend/src/main.tsx
@@ -3,7 +3,7 @@ import "./index.css"
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-import App from "./application"
+import App from "./app"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>

c1ee80242141520bd520d2933620da447b7ad1b8
Luca Airinei <illustrofia@gmail.com>
2024-04-06 14:24:37 +0200

    chore: fix eslint

--- apps/todo/frontend/.eslintrc.cjs
+++ apps/todo/frontend/.eslintrc.cjs
@@ -1,6 +1,4 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true, node: true },
   extends: [
     "@incubator/eslint-config/react.cjs",
     "plugin:@tanstack/eslint-plugin-query/recommended",

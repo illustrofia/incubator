c266103b258e32d34b96044891a8bbcc2e561575
Luca Airinei <illustrofia@gmail.com>
2024-04-09 18:26:01 +0200

    chore(todo): attempt to fix cors issues

--- apps/todo/backend/src/utils/getOrigin.ts
+++ apps/todo/backend/src/utils/getOrigin.ts
@@ -1,9 +1,9 @@
 export const getOrigin = () => {
-  if (process.env.NODE_ENV === "production" && !process.env.DOMAIN) {
-    throw new Error("DOMAIN is not set")
+  if (process.env.NODE_ENV === "production" && !process.env.FRONTEND_PROD_URL) {
+    throw new Error("FRONTEND_PROD_URL is not set")
   }
 
   return process.env.NODE_ENV === "production"
-    ? process.env.DOMAIN!
+    ? process.env.FRONTEND_PROD_URL!
     : "localhost"
 }

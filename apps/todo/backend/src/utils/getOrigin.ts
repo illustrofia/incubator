24cfd23a8d54403488e952bfead13af06a842843
Luca Airinei <illustrofia@gmail.com>
2024-04-11 23:30:26 +0200

    chore(todo): update frontend port

--- apps/todo/backend/src/utils/getOrigin.ts
+++ apps/todo/backend/src/utils/getOrigin.ts
@@ -5,5 +5,5 @@ export const getOrigin = () => {
 
   return process.env.NODE_ENV === "production"
     ? process.env.FRONTEND_PROD_URL!
-    : "http://localhost:8080"
+    : "http://localhost:5173"
 }
